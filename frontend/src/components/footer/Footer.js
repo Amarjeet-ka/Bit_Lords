fe25937cea458b1839444f24c0c907166ab97fd8
Amarjeet-ka <116912077+Amarjeet-ka@users.noreply.github.com>
2023-06-02 16:26:20 +0530

    feature/added icons

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+
 
 export default function Footer() {
   return (
     <MDBFooter style={{backgroundColor:'#04d3f7'}} className='text-center text-lg-start text-muted'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks:</span>
+          <span>Get connected with us on social networks:   <FaInstagram size={28} />  <FaTwitter size={28} />    <FaYoutube size={28} /></span>
         </div>
 
-        <div>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="facebook-f" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="twitter" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="google" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="instagram" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="linkedin" />
-          </a>
-          <a href='' className='me-4 text-reset'>
-            <MDBIcon fab icon="github" />
-          </a>
-        </div>
+       
       </section>
 
       <section className=''>
