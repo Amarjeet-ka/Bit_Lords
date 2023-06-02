5f42fa4c295c473aa9b5f2b9ab27e8129c96d193
Amarjeet-ka <116912077+Amarjeet-ka@users.noreply.github.com>
2023-06-02 13:03:31 +0530

    feature/ added pages to apps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
-
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import Community from './pages/Community';
+import Skill from './pages/Skill';
+import Career from './pages/Career';
+import Personal from './pages/Personal';
+import Resourse from './pages/Resourse';
+import Tutorials from './pages/Tutorials';
+import Cources from './pages/Cources';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Homepage/>} />
+    <Route path="/" element={<Login/>} />
+    <Route path="/community" element={<Community/>} />
+    <Route path="/skills" element={<Skill/>} />
+    <Route path="/career" element={<Career/>} />
+    <Route path="/personal" element={<Personal/>} />
+    <Route path="/resource" element={<Resourse/>} />
+    <Route path="/tutorials" element={<Tutorials/>} />
+    <Route path="/course" element={<Cources/>} />
+  </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Cources.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Cources() {
+  return (
+    <div>Cources</div>
+  )
+}
