2947f0a2c738bc7b88148ef37c837ccda5d10269
Amarjeet-ka <116912077+Amarjeet-ka@users.noreply.github.com>
2023-06-02 14:40:18 +0530

    feature/added header

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react'
 import Navigation from '../components/navbar/Navbar'
+import Header from '../components/header/Header'
 
 export default function Homepage() {
   return (  
  <Fragment>
 <Navigation/>
  
+ <Header/>
  </Fragment>
 
 )
