0152cf6efd30875d124e04f5860ba202d77112ee
NaveenGond <109409941+NaveenGond@users.noreply.github.com>
2023-06-02 15:38:40 +0530

    skills added

--- frontend/src/pages/Skill.jsx
+++ frontend/src/pages/Skill.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Navigation from '../components/navbar/Navbar'
+import Stream from '../components/stream/Stream'
 
 export default function Skill() {
   return (
-    <div>Skill</div>
+     <Fragment>
+       <Navigation/>
+    <Stream/>
+     </Fragment>
   )
 }
